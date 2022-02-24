04000e6fb91fec42e931da2e65eb74b47c7dc7f9
quan12346789 <100343780+quan12346789@users.noreply.github.com>
2022-02-24 16:28:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+    $a = 5;
+    $b = 3;
+    $tong = $a + $b;
+    echo "Tong: $tong <br>";
+
+    $hieu = $a - $b;
+    echo "Hieu: $hieu <br>";
+
+    $tich = $a * $b;
+    echo "Tich: $tich <br>";
+
+    $thuong = $a / $b;
+    echo "Thuong: $thuong <br>";
+
+    $sodu = $a % $b;
+    echo "So du: $sodu <br>";
+?>
